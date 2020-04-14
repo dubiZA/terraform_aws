3904c23411baacee565b86af1f68f037cba112d8
dubiZA <iamdzh@outlook.com>
2020-04-14 06:42:35 -0600

    Remove single instance and add ASG config

--- main.tf
+++ main.tf
@@ -3,16 +3,26 @@ provider "aws" {
   profile = "terraform"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+
+}
+
+
 variable "server_port" {
   description = "The port used by the infrastructure for HTTP requests"
   type = number
   default = 8080
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-07ebfd5b3428b6f4d"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-07ebfd5b3428b6f4d"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
   
   user_data = <<-EOF
               #!/bin/bash
@@ -20,16 +30,25 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
   
-  tags = {
-    Name = "terraform-example"
+  # Required when using a launch config with an ASG.
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public IP of the server"
-}
+resource "aws_autoscaling_group" "name" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
 
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform_asg_example"
+    propagate_at_launch = true
+  }
+}
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
